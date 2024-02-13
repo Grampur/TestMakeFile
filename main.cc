1996522d34cc07f7aabcebcf84317bd9466f31de
Gagan <grampur123@gmail.com>
2024-02-13 11:00:07 -0800

    2nd try

--- main.cc
+++ main.cc
@@ -12,6 +12,7 @@
  */
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
@@ -19,6 +20,40 @@ int main()
     // Your code should be here.
     // The following is a just sample statement.
     // cout << "Hello world!" << endl;
+
+    int size1;
+    vector <int> vec;
+
+    cin >> size1;
+
+    for(int i = 0; i < size1; i++){
+        int temp;
+        cin >> temp;
+        vec.push_back(temp);
+    }
+
+    int size2;
+    vector <int> vec2;
+
+    cin >> size2;
+
+    for(int i = 0 ; i < size2; i++){
+        int temp;
+        cin >> temp;
+        vec2.push_back(temp);
+    }
+
+    for(int i = 0; i < size1; i++){
+        cout << vec[i] << " ";
+    }
+
+    cout << endl;
+
+    for(int i = 0; i < size2; i++){
+        cout << vec2[i] << " ";
+    }
+
+    cout << endl;
 
     cout << "Answer:20 15 14 -7" << endl;
     return 0;
